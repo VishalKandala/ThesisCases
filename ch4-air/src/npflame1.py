6237a4b81ba39de9bafb1bf936aa6fdd287bff79
vishalkandala <vishalkandala@tamu.edu>
2020-10-27 03:35:33 +0000

    grid_iterations added

--- ch4-air/src/npflame1.py
+++ ch4-air/src/npflame1.py
@@ -25,9 +25,9 @@ mdot_f     =   1                    # kg/m^2/s
 comp_o       =  'O2:0.21, N2:0.78, AR:0.01';   # air composition
 comp_f       =  'CH4:1';                      # fuel composition
 
-# distance between inlets is 2 cm; start with an evenly-spaced 25-point
+# distance between inlets is 2 cm; start with an evenly-spaced 50-point
 # grid
-initial_grid = np.linspace(0,0.02,num=25)
+grid_iterations=[25,50,100,200]
 
 tol_ss    = [1.0e-5, 1.0e-9]        # [rtol, atol] for steady-state
                                     # problem
@@ -36,8 +36,14 @@ tol_ts    = [1.0e-3, 1.0e-9]        # [rtol, atol] for time stepping
 loglevel  = 1                       # amount of diagnostic output (0
                                     # to 5)				    
 refine_grid = 0                     # 1 to enable refinement, 0 to
-                                    # disable 				   
 
+plt.title("Temperature Profile of Methane air Diffusion flame")
+
+for i in range(len(grid_iterations)):
+
+	initial_grid = np.linspace(0,0.02,num=grid_iterations[i])
+
+                                   # disable 				   
 
 ################ create the gas object ########################
 #
@@ -50,41 +56,41 @@ refine_grid = 0                     # 1 to enable refinement, 0 to
 # IdealGasMix('mech.cti') to read a mechanism in Cantera format.  If
 # you need to convert from Chemkin format, use the ck2cti utility
 # program first.
-gas = GRI30('Mix')
+	gas = GRI30('Mix')
 
 # create an object representing the counterflow flame configuration,
 # which consists of a fuel inlet on the left, the flow in the middle,
 # and the oxidizer inlet on the right. Class CounterFlame creates this
 # configuration.
 
-f = CounterFlame(gas = gas, grid = initial_grid)
+	f = CounterFlame(gas = gas, grid = initial_grid)
 
 # Set the state of the two inlets
 
-f.fuel_inlet.set(massflux = mdot_f,
+	f.fuel_inlet.set(massflux = mdot_f,
                  mole_fractions = comp_f,
                  temperature = tin_f)
 
-f.oxidizer_inlet.set(massflux = mdot_o,
+	f.oxidizer_inlet.set(massflux = mdot_o,
                      mole_fractions = comp_o,
                      temperature = tin_o)
 
 # set the error tolerances
-f.set(tol = tol_ss, tol_time = tol_ts)
+	f.set(tol = tol_ss, tol_time = tol_ts)
 
 # construct the initial solution estimate. To do so, it is necessary
 # to specify the fuel species. If a fuel mixture is being used,
 # specify a representative species here for the purpose of
 # constructing an initial guess.
-f.init(fuel = 'CH4')
+	f.init(fuel = 'CH4')
 
 # show the starting estimate
-f.showSolution()
+	f.showSolution()
 
 # First disable the energy equation and solve the problem without
 # refining the grid
-f.set(energy = 'off')
-f.solve(loglevel, 0)
+	f.set(energy = 'off')
+	f.solve(loglevel, 0)
 
 # Now specify grid refinement criteria, turn on the energy equation,
 # and solve the problem again. The ratio parameter controls the
@@ -94,37 +100,36 @@ f.solve(loglevel, 0)
 # will be removed if the relative slope and curvature for all
 # components fall below the prune level. Set prune < min(slope,
 # curve), or to zero to disable removing grid points.
-f.setRefineCriteria(ratio = 200.0, slope = 0.1, curve = 0.2, prune = 0.02)
-f.set(energy = 'on')
-f.solve(1)
+	f.setRefineCriteria(ratio = 200.0, slope = 0.1, curve = 0.2, prune = 0.02)
+	f.set(energy = 'on')
+	f.solve(1)
 
 # Save the solution
-f.save('../results/npflame1.xml')
+	f.save('../results/npflame1.xml')
 
 # write the velocity, temperature, and mole fractions to a CSV file
-z = f.flame.grid()
-T = f.T()
-u = f.u()
-V = f.V()
-fcsv = open('../results/npflame1.csv','w')
-writeCSV(fcsv, ['z (m)', 'u (m/s)', 'V (1/s)', 'T (K)']
+	z = f.flame.grid()
+	T = f.T()
+	u = f.u()
+	V = f.V()
+	fcsv = open('../results/npflame1.csv','w')
+	writeCSV(fcsv, ['z (m)', 'u (m/s)', 'V (1/s)', 'T (K)']
          + list(gas.speciesNames()))
-for n in range(f.flame.nPoints()):
-    f.setGasState(n)
-    writeCSV(fcsv, [z[n], u[n], V[n], T[n]]+list(gas.moleFractions()))
-fcsv.close()
+	for n in range(f.flame.nPoints()):
+    		f.setGasState(n)
+    		writeCSV(fcsv, [z[n], u[n], V[n], T[n]]+list(gas.moleFractions()))
+		fcsv.close()
 		
-print 'solution saved to npflame1.csv'
+	print 'solution saved to npflame1.csv'
 
-f.showSolution()
-f.showStats()
+	f.showSolution()
+	f.showStats()
+	plt.plot(z,T,label='n='+str(grid_iterations[i]))
 
-temperatureplot=plt.figure()
-plt.title("Temperature Profile of Methane air Diffusion flame")
-plt.plot(z,T)
 plt.xlim(0.000, 0.020)
 plt.grid(True)
 plt.ylim(0,2500)
 plt.xlabel('Z(m)')
 plt.ylabel('T(K)')
+plt.legend()
 plt.savefig('../plots/zvT.png')
